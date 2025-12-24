52ee4d5864ec4057717016041cf8eb19033121a4
Ebuka24Stephen <sebuka559@gmail.com>
2025-12-24 19:47:29 +0100

    fix: correct chat URL path in urlpatterns

--- chat/urls.py
+++ chat/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
-    path('api/chat/', include('messaging.urls'))
+    path('chat/', include('messaging.urls'))
 ]
