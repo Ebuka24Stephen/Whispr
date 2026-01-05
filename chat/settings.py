cdc7e8f3889a8b0ab0e3ef842c5bfa840cff5f0b
Ebuka24Stephen <sebuka559@gmail.com>
2026-01-05 15:59:50 +0100

    fix: update database settings to use PostgreSQL and add psycopg2-binary dependency

--- chat/settings.py
+++ chat/settings.py
@@ -71,10 +71,19 @@ ASGI_APPLICATION = 'chat.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT", cast=int),
+        "CONN_MAX_AGE": 600,
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
