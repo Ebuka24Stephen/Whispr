364cb8e74a8a3568a34133fdf8cc5ef46dfc4330
Ebuka24Stephen <sebuka559@gmail.com>
2026-01-07 16:32:04 +0100

    fix: optimize ASGI application setup by reusing get_asgi_application() instance

--- chat/asgi.py
+++ chat/asgi.py
@@ -1,13 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import messaging.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             messaging.routing.websocket_urlpatterns
